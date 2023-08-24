2a1ce9ae51f1a198bf5cb89a3e7be98598afb5a9
Ben Burns <803016+benjamincburns@users.noreply.github.com>
2023-08-24 10:53:46 +0000

    Make an attempt at using ray instead of multiprocessing

--- rlgym_ppo/batched_agents/batched_agent.py
+++ rlgym_ppo/batched_agents/batched_agent.py
@@ -1,81 +1,51 @@
 from typing import Union
+import ray
+import time
+import gym
 
+@ray.remote
+class BatchedAgent(object):
+    def __init__(self, build_env_fn, seed, render, render_delay: Union[float, None]):
+        """
+        :param seed: Seed for environment and action space randomization.
+        :param render: Whether the environment will be rendered every timestep.
+        :param render_delay: Amount of time in seconds to delay between steps while rendering.
+        """
+        self.env = build_env_fn()
+        self.seed = seed
+        self.render=render
+        self.render_delay=render_delay
 
-def batched_agent_process(pipe, seed, render, render_delay: Union[float, None]):
-    """
-    Function to interact with an environment and communicate with the learner through a pipe.
+        # Seed everything.
+        self.env.action_space.seed(seed)
+    
+    def step(self, action):
+        obs, rew, done, _ = self.env.step(action)
+        if done:
+            obs = self.env.reset()
 
-    :param pipe: A bidirectional communication pipe.
-    :param seed: Seed for environment and action space randomization.
-    :param render: Whether the environment will be rendered every timestep.
-    :param render_delay: Amount of time in seconds to delay between steps while rendering.
-    :return: None
-    """
+        if self.render:
+            self.env.render()
+            if self.render_delay is not None:
+                time.sleep(self.render_delay)
 
-    import time
-    import gym
-    env = None
+        return obs, rew, done
 
-    # Wait for initialization data from the learner.
-    while env is None:
-        data = pipe.recv()
-        if data[0] == "initialization_data":
-            build_env_fn = data[1]
+    
+    def reset(self):
+        return self.env.reset()
+    
+    def get_env_shapes(self):
+        t = type(self.env.action_space)
+        action_space_type = "discrete"
+        if t == gym.spaces.multi_discrete.MultiDiscrete:
+            action_space_type = "multi-discrete"
+        elif t == gym.spaces.box.Box:
+            action_space_type = "continuous"
 
-            env = build_env_fn()
+        if hasattr(self.env.action_space, "n"):
+            n_acts = self.env.action_space.n
+        else:
+            n_acts = self.env.action_space.shape
 
-    # Seed everything.
-    env.action_space.seed(seed)
-    reset_state = env.reset()
-    pipe.send(("reset_state", reset_state))
-
-    # Primary interaction loop.
-    try:
-        while True:
-            data = pipe.recv()
-
-            # If we have an action, take a step in the environment and transmit the timestep data back to the learner.
-            if data[0] == "action":
-                action = data[1]
-                obs, rew, done, _ = env.step(action)
-                if done:
-                    obs = env.reset()
-
-                done = 1 if done else 0
-
-                pipe.send(("env_step_data", (obs, rew, done)))
-                if render:
-                    env.render()
-                    if render_delay is not None:
-                        time.sleep(render_delay)
-
-            # If the learner is requesting obs and action space shapes, provide them.
-            elif data[0] == "get_env_shapes":
-                t = type(env.action_space)
-                action_space_type = "discrete"
-                if t == gym.spaces.multi_discrete.MultiDiscrete:
-                    action_space_type = "multi-discrete"
-                elif t == gym.spaces.box.Box:
-                    action_space_type = "continuous"
-
-                if hasattr(env.action_space, "n"):
-                    n_acts = env.action_space.n
-                else:
-                    n_acts = env.action_space.shape
-
-                print("Received request for env shapes, returning",env.observation_space.shape, n_acts, action_space_type)
-                pipe.send(("env_shapes", (env.observation_space.shape, n_acts, action_space_type)))
-
-            elif data[0] == "stop":
-                break
-
-    # Catch everything and print it.
-    except:
-        import traceback
-        print("ERROR IN BATCHED AGENT LOOP")
-        traceback.print_exc()
-
-    # Close the pipe and local environment instance.
-    finally:
-        pipe.close()
-        env.close()
+        return self.env.observation_space.shape, n_acts, action_space_type
--- rlgym_ppo/batched_agents/batched_agent_manager.py
+++ rlgym_ppo/batched_agents/batched_agent_manager.py
@@ -8,32 +8,29 @@ Description:
     the trajectories from each instance of the environment.
 """
 
-import multiprocessing as mp
 import time
-from multiprocessing.connection import PipeConnection
 from typing import Union
 
 import numpy as np
 import torch
 
 from rlgym_ppo.batched_agents import BatchedTrajectory
-from rlgym_ppo.batched_agents.batched_agent import batched_agent_process
+from rlgym_ppo.batched_agents.batched_agent import BatchedAgent
 
+import ray
 
 class BatchedAgentManager(object):
     def __init__(self, policy, min_inference_size=8, seed=123):
         self.policy = policy
         self.seed = seed
-        self.processes: dict[
-            int, tuple[mp.Process, PipeConnection, PipeConnection]
-        ] = {}
+        self.batched_agents = []
+        self.n_procs = 0
         self.current_obs = []
-        self.current_pids = []
         self.average_reward = None
         self.cumulative_timesteps = 0
         self.min_inference_size = min_inference_size
-        self.ep_rews = {}
-        self.trajectory_map = {}
+        self.ep_rews = [] 
+        self.trajectories = {}
         self.prev_time = 0
 
     def collect_timesteps(self, n):
@@ -63,31 +60,13 @@ class BatchedAgentManager(object):
         truncated = []
 
         n_collected = 0
-        n_procs = max(1, len(list(self.processes.keys())))
-        n_obs_per_inference = min(self.min_inference_size, n_procs)
 
         # Collect n timesteps.
         while n_collected < n:
-            next_obs = []
-            next_pids = []
-
-            # Send actions for the current observations and collect new states to act on. Note that the next states
-            # will not necessarily be from the environments that we just sent actions to. Whatever timestep data happens
-            # to be lying around in the buffer will be collected and used in the next inference step.
-            self._send_actions()
-            n_collected_since_inference = 0
-            while n_collected_since_inference < n_obs_per_inference:
-                n_collected_right_now = self._collect_responses(next_obs, next_pids)
-
-                n_collected_since_inference += n_collected_right_now
-                n_collected += n_collected_right_now
-
-            self.current_obs = next_obs
-            self.current_pids = next_pids
-            self._sync_trajectories()
+            n_collected += self._step()
 
         # Organize our new timesteps into the appropriate lists.
-        for pid, trajectory in self.trajectory_map.items():
+        for trajectory in self.trajectories:
             trajectories = trajectory.get_all()
             if len(trajectories) == 0:
                 continue
@@ -128,12 +107,8 @@ class BatchedAgentManager(object):
             t2 - t1,
         )
 
-    def _sync_trajectories(self):
-        for pid, trajectory in self.trajectory_map.items():
-            trajectory.update()
-
     @torch.no_grad()
-    def _send_actions(self):
+    def _step(self):
         """
         Send actions to environment processes based on current observations.
         """
@@ -148,64 +123,49 @@ class BatchedAgentManager(object):
         actions = actions.view((shape[0], shape[1], -1)).numpy()
         log_probs = log_probs.view((shape[0], shape[1], -1)).numpy()
 
-        for i in range(len(self.current_pids)):
-            state = np.asarray(self.current_obs[i])
+        results = ray.get([agent.step.remote(actions[i]) for i, agent in enumerate(self.batched_agents)])
+        prev_obs = self.current_obs
+        self.current_obs = []
+        
+        n_collected = 0
+
+        for i, trajectory in enumerate(self.trajectories):
             action = actions[i]
             log_prob = log_probs[i]
-            pid = self.current_pids[i]
-
-            self.processes[pid][1].send(("action", action))
-            self.trajectory_map[pid].action = action
-            self.trajectory_map[pid].log_prob = log_prob
-            self.trajectory_map[pid].state = state
-        self.current_obs = None
-
-    def _collect_responses(self, next_obs, next_pids):
-        """
-        Collect responses from environment processes and update trajectory data.
-        :param next_obs: List to store next observations.
-        :param next_pids: List to store process IDs.
-        :return: Number of responses collected.
-        """
-
-        n_collected = 0
-        for proc_id, proc_package in self.processes.items():
-            process, parent_end, child_end = proc_package
-            if not parent_end.poll():
-                continue
-            available_data = parent_end.recv()
-            header, data = available_data
-
-            if header == "env_step_data":
-                next_pids.append(proc_id)
-                next_observation, rew, done = data
-
-                if type(rew) in (list, tuple, np.ndarray):
-                    n_collected += len(rew)
-                    for i in range(len(rew)):
-                        if i >= len(self.ep_rews[proc_id]):
-                            self.ep_rews[proc_id].append(rew[i])
-                        else:
-                            self.ep_rews[proc_id][i] += rew[i]
-                else:
-                    n_collected += 1
-                    self.ep_rews[proc_id][0] += rew
-
-                if done:
-                    if self.average_reward is None:
-                        self.average_reward = self.ep_rews[proc_id][0]
+            state = np.asarray(prev_obs[i])
+            next_state, reward, done = results[i]
+            
+            if type(reward) in (list, tuple, np.ndarray):
+                n_collected += len(reward)
+                for j in range(len(reward)):
+                    if j >= len(self.ep_rews[i]):
+                        self.ep_rews[i].append(reward[j])
                     else:
-                        for ep_rew in self.ep_rews[proc_id]:
-                            self.average_reward = (
-                                self.average_reward * 0.9 + ep_rew * 0.1
-                            )
+                        self.ep_rews[i][j] += reward[j]
+            else:
+                n_collected += 1
+                self.ep_rews[i][0] += reward
+            
+            if done:
+                if self.average_reward is None:
+                    self.average_reward = self.ep_rews[i][0]
+                else:
+                    for ep_rew in self.ep_rews[i]:
+                        self.average_reward = (
+                            self.average_reward * 0.9 + ep_rew * 0.1
+                        )
+                
+                self.ep_rews[i] = [0]
+                
+            trajectory.action = action
+            trajectory.log_prob = log_prob
+            trajectory.state = state
+            trajectory.reward = reward
+            trajectory.next_state = next_state
+            trajectory.done = done
+            trajectory.update()
+            self.current_obs.append(next_state)
 
-                    self.ep_rews[proc_id] = [0]
-
-                next_obs.append(next_observation)
-                self.trajectory_map[proc_id].reward = rew
-                self.trajectory_map[proc_id].next_state = next_observation
-                self.trajectory_map[proc_id].done = done
         return n_collected
 
     def _get_initial_states(self):
@@ -214,18 +174,7 @@ class BatchedAgentManager(object):
         :return: None.
         """
 
-        self.current_obs = []
-        self.current_pids = []
-        for proc_id, proc_package in self.processes.items():
-            process, parent_end, child_end = proc_package
-            while not parent_end.poll():
-                time.sleep(0.01)
-
-            available_data = parent_end.recv()
-            header, data = available_data
-            if header == "reset_state":
-                self.current_pids.append(proc_id)
-                self.current_obs.append(data)
+        self.current_obs = ray.get([agent.reset.remote() for agent in self.batched_agents])
 
     def _get_env_shapes(self):
         """
@@ -234,25 +183,7 @@ class BatchedAgentManager(object):
         """
 
         print("Requesting env shapes...")
-        process, parent_end, child_end = self.processes[0]
-        parent_end.send(("get_env_shapes", None))
-        obs_shape, action_shape, action_space_type = None, None, None
-        done = False
-
-        while not done:
-            while not parent_end.poll():
-                time.sleep(0.1)
-            available_data = parent_end.recv()
-            header, data = available_data
-            if header == "env_shapes":
-                print("Received env shapes from child process")
-                obs_shape, action_shape, action_space_type = data
-                done = True
-
-        obs_shape: Union[int, None]
-        action_shape: Union[int, None]
-        action_space_type: Union[str, None]
-        return obs_shape, action_shape, action_space_type
+        return ray.get(self.batched_agents[0].get_env_shapes.remote())
 
     def init_processes(
         self,
@@ -272,50 +203,26 @@ class BatchedAgentManager(object):
         :param render_delay: A period in seconds to delay a process between frames while rendering.
         :return: A tuple containing observation shape, action shape, and action space type.
         """
+        
+        self.n_procs = n_processes
 
-        can_fork = "forkserver" in mp.get_all_start_methods()
-        start_method = "forkserver" if can_fork else "spawn"
-        context = mp.get_context(start_method)
-
+        self.batched_agents = []
         for i in range(n_processes):
             render_this_proc = i == 0 and render
-            parent_end, child_end = context.Pipe()
-            process = context.Process(
-                target=batched_agent_process,
-                args=(child_end, self.seed + i, render_this_proc, render_delay),
-            )
-            process.start()
-            parent_end.send(("initialization_data", build_env_fn))
-            self.processes[i] = (process, parent_end, child_end)
+            self.batched_agents.append(
+                BatchedAgent.remote(
+                build_env_fn,
+                self.seed + i,
+                render_this_proc,
+                render_delay
+            ))
             if spawn_delay is not None:
                 time.sleep(spawn_delay)
 
-        self.ep_rews = {pid: [0] for pid in self.processes.keys()}
-        self.trajectory_map = {
-            pid: BatchedTrajectory() for pid in self.processes.keys()
-        }
+        self.ep_rews = [[0] for _ in range(n_processes)]
+        self.trajectories = [BatchedTrajectory() for _ in range(n_processes)]
         self._get_initial_states()
         return self._get_env_shapes()
 
     def cleanup(self):
-        """
-        Clean up resources and terminate processes.
-        """
-        for proc_id, proc_package in self.processes.items():
-            process, parent_end, child_end = proc_package
-            try:
-                parent_end.send(("stop", None))
-            except:
-                print("Failed to send stop signal to child process!")
-            try:
-                parent_end.close()
-            except:
-                print("Unable to close parent connection")
-            try:
-                child_end.close()
-            except:
-                print("Unable to close child connection")
-            try:
-                process.join()
-            except:
-                print("Unable to join process")
+        ray.shutdown()
